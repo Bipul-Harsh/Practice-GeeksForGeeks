5e2196830fa38765beea162da7bca357cda7334e
Bipul Harsh <bipulharsh123@gmail.com>
2022-05-17 22:48:56 +0530

    2-E

---
+++ LuckyNumber.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User-function template for C++
+
+// Complete the function
+// n: Input n
+// Return True if the given number is a lucky number else return False
+
+class Solution{
+public:
+    bool isLucky(int n) {
+        // code here
+        /**
+         * Here take n as pos and check if that position is removable 
+         * if not, get its new position after removing the other removable element.
+         * 
+        */
+        
+        if(n<2)
+            return true;
+        
+        int remPos = 2;
+        while(n >= remPos){
+            if(n%remPos == 0)
+                return false;
+            
+            n -= n/remPos;
+            remPos++;
+        }
+        
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+signed main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling isLucky() function
+        if(obj.isLucky(n))
+            cout<<"1\n";//printing "1" if isLucky() returns true
+        else
+            cout<<"0\n";//printing "0" if isLucky() returns false
+    }
+    
+}  // } Driver Code Ends
