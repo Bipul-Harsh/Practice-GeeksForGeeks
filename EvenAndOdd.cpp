bb398efe70d8394bcd000bc6cd875d9b47ab4378
Bipul Harsh <bipulharsh123@gmail.com>
2022-05-26 17:30:58 +0530

    1-E

---
+++ EvenAndOdd.cpp
@@ -0,0 +1,79 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    void reArrange(int arr[], int N) {
+        // code here
+        int evenInd = 0;
+        int oddInd = 1;
+        while(evenInd<N && oddInd<N){
+            while(arr[evenInd]%2==0)
+                evenInd += 2;
+            
+            while(arr[oddInd]%2==1)
+                oddInd += 2;
+            
+            swap(arr[evenInd], arr[oddInd]);
+        }    cout<<arr[ind]<<' '<<endl;
+    }
+};
+
+// { Driver Code Starts.
+
+int check(int arr[], int n)
+{
+    int flag = 1;
+    int c=0, d=0;
+    for(int i=0; i<n; i++)
+    {
+        if(i%2==0)
+        {
+            if(arr[i]%2)
+            {
+                flag = 0;
+                break;
+            }
+            else
+                c++;
+        }
+        else
+        {
+            if(arr[i]%2==0)
+            {
+                flag = 0;
+                break;
+            }
+            else
+                d++;
+        }
+    }
+    if(c!=d)
+        flag = 0;
+        
+    return flag;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int arr[N];
+        for(int i=0; i<N; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        ob.reArrange(arr,N);
+        
+        cout<<check(arr,N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
