482f2468ec5f39d50cd0103f0e150cc7db1b4718
Bipul Harsh <bipulharsh123@gmail.com>
2022-06-28 22:00:17 +0530

    1-E

---
+++ SumOfLeafNodes.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+int sumLeaf(Node* root);
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout<<sumLeaf(root)<<endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/*Structure of the node of the binary tree is as
+struct Node
+{
+	int data;
+	Node *left, *right;
+};*/
+// function should return the sum of all the 
+// leaf nodes of the binary tree 
+void sumLeaf(Node *ptr, int &res){
+    if(!ptr->left && !ptr->right){
+        res+=ptr->data;
+        return;
+    }
+    if(ptr->left)
+        sumLeaf(ptr->left, res);
+    if(ptr->right)
+        sumLeaf(ptr->right, res);
+}
+int sumLeaf(Node* root)
+{
+    // Code here
+    int ans=0;
+    sumLeaf(root, ans);
+    return ans;
+}
