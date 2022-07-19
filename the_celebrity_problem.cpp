8e6a068f291378ccf4abf26e58a518f86362ec5a
Bipul Harsh <bipulharsh123@gmail.com>
2022-07-19 22:41:15 +0530

    1-M

---
+++ the_celebrity_problem.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        // code here 
+        for(int row=0; row<n; row++){
+            for(int col=0; col<n; col++){
+                if(M[row][col]){
+                    M[col][col]++;
+                    M[row][row]--;
+                }
+            }
+        }
+        
+        for(int ind=0; ind<n; ind++){
+            if(M[ind][ind] == n-1)
+                return ind;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+
+// } Driver Code Ends
