39eb3edcd3cf3c7caf601d34cd608f05abf75275
Bipul Harsh <bipulharsh123@gmail.com>
2022-08-13 20:01:55 +0530

    1-B

---
+++ ToggleBitsGiveRange.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int toggleBits(int N , int L , int R) {
+        // code here
+        int res = (1<<(L-1))-1;
+        int res2 = (1<<R)-1;
+        int temp = res ^ res2;
+        
+        return N^temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,L,R;
+        
+        cin>>N>>L>>R;
+
+        Solution ob;
+        cout << ob.toggleBits(N,L,R) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
