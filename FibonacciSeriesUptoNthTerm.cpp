be5fb3fe6cdf38f31bea017ae5cacd778a2cb6d4
Bipul Harsh <bipulharsh123@gmail.com>
2022-09-12 23:35:19 +0530

    1-E

---
+++ FibonacciSeriesUptoNthTerm.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<long long> Series(int N) {
+        // COde here
+        vector<long long> ans = {0, 1};
+        int count = 2;
+        while(count <= N){
+            ans.push_back(ans[count-2] + ans[count-1]);
+            ++count;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution obj;
+
+        vector<long long> ans = obj.Series(n);
+        for (auto x : ans) cout << x << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
