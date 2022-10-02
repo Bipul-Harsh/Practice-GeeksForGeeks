1dfbce9978ec8e547038a49ab6a1e38e8bbfc4ca
Bipul Harsh <bipulharsh123@gmail.com>
2022-10-02 17:55:10 +0530

    1-E

---
+++ MoveAllZerosToEndOfArray.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    // code here
+	    int start;
+	    for(start=0; start<n; start++){
+	        if(arr[start] == 0)
+	            break;
+	    }
+	    
+	    for(int ind=start+1; ind<n; ind++){
+	        if(arr[ind]){
+	            swap(arr[start], arr[ind]);
+	            ++start;
+	        }
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.pushZerosToEnd(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
