0d0dfbae38715c6a949e2c89fda0b3571b3ad6dc
Bipul Harsh <bipulharsh123@gmail.com>
2022-10-03 18:57:35 +0530

    1-E

---
+++ PerfectNumbers.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int isPerfectNumber(long long N) {
+        // code here
+        if(N==1)
+            return 0;
+        int sum = 1;
+        for(int val=2; val<=sqrt(N); val++)
+            if(N%val == 0){
+                sum += val;
+                sum += N/val;
+            }
+        return sum == N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.isPerfectNumber(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
