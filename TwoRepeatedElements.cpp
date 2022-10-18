b757eaf3bd4817ad0e90695e05560e6de38dd9c7
Bipul Harsh <bipulharsh123@gmail.com>
2022-10-18 23:34:15 +0530

    1-E

---
+++ TwoRepeatedElements.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to find two repeated elements.
+    vector<int> twoRepeated (int arr[], int N) {
+        // Your code here
+        vector<int>res;
+        
+        for(int ind=0; ind<N+2; ind++){
+            arr[abs(arr[ind])-1] *= -1;
+            
+            if(arr[abs(arr[ind])-1] > 0)
+                res.push_back(abs(arr[ind]));
+        }
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        
+        int a[n+2];
+        
+        for(int i=0;i<n+2;i++)
+            cin>>a[i];
+        
+        Solution obj;
+        vector<int> res;
+        res = obj.twoRepeated(a, n);
+        cout<<res[0]<<" "<<res[1]<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
