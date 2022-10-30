323f2f6752022e89e72bd619d7978787a0900cb0
Bipul-Harsh <bipulharsh123@gmail.com>
2022-10-30 23:03:22 +0530

    1-E

---
+++ MinOperations.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int solve(int a, int b) {
+        // code here
+        int res=0;
+        if((a&b) != a)
+            ++res;
+        if((a&b) != b)
+            ++res;
+        
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.solve(a, b);
+        
+        printf("%d\n", res);
+        
+    }
+}
+
+// } Driver Code Ends
