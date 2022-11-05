3401fa8df7089456f8bcf68f0e74c17b7f276b8d
Bipul-Harsh <bipulharsh123@gmail.com>
2022-11-05 23:37:34 +0530

    1-E

---
+++ SortBySetBitCount.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public: 
+    void sortBySetBitCount(int arr[], int n)
+    {
+        // Your code goes here
+        stable_sort(arr, arr+n, [](int v1, int v2){
+            int c1 = __builtin_popcount(v1);
+            int c2 = __builtin_popcount(v2);
+            return c1 > c2;
+        });
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        ob.sortBySetBitCount(arr, n);
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
