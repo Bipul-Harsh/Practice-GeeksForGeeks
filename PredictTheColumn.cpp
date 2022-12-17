07affbe91f77ffe357a0a48a0bf9ea0cfe46bef6
Bipul-Harsh <bipulharsh123@gmail.com>
2022-12-17 18:24:16 +0530

    1-E

---
+++ PredictTheColumn.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    /*Function to count zeros in each column
+    * N : Number of rows and columns in array
+    M is the matrix that is globally declared
+    */
+    int columnWithMaxZeros(vector<vector<int>>arr,int N){
+        
+        // Your code here
+        int maxCnt = 0;
+        int maxInd=0;
+        for(int c = 0; c<N; c++){
+            int cnt = 0;
+            for(int r = 0; r<N; r++){
+                if(arr[r][c] == 0)
+                    ++cnt;
+            }
+            if(cnt > maxCnt){
+                maxCnt = cnt;
+                maxInd = c;
+            }
+        }
+        
+        return maxInd;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    vector<vector<int>>arr(N,vector<int>(N));
+	    // Taking elements input into the matrix
+	    for(int i = 0;i<N;i++){
+	        for(int j = 0;j<N;j++){
+	            int x;
+	            cin >> x; 
+	            arr[i][j]=x;
+	        }
+	    }
+	    Solution ob;
+	    cout << ob.columnWithMaxZeros(arr,N) << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
