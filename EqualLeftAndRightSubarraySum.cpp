ea53321e46ec04ff52a906e957a20022c5a767b6
Bipul-Harsh <bipulharsh123@gmail.com>
2023-01-04 23:37:11 +0530

    1-E

---
+++ EqualLeftAndRightSubarraySum.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int equalSum(int N, vector<int> &A) {
+        // code here
+        long long unsigned s1 = 0;
+        long long unsigned s2 = 0;
+        for(int val : A){
+            s1 += val;
+        }
+        
+        for(int ind=0; ind<N && s1 >= s2; ind++){
+            s1 -= A[ind];
+            if(s1 == s2)
+                return ind+1;
+            s2 += A[ind];
+        }
+        
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N; 
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.equalSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
