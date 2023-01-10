791f5441bafc82bda8b0ac3cf48518f6a0e4fba6
Bipul-Harsh <bipulharsh123@gmail.com>
2023-01-10 22:43:55 +0530

    1-M

---
+++ MaximumIntersectingLines.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxIntersections(vector<vector<int>> lines, int N) {
+        // Code here
+        map<int, int>m;
+        for(auto cord : lines){
+            int p1 = cord[0], p2 = cord[1]+1;
+            ++m[p1];
+            --m[p2];
+        }
+        
+        int ans = 0, sum = 0;
+        for(auto pt : m){
+            sum += pt.second;
+            ans = max(ans, sum);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N, vector<int>(2));
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < N; i++) {
+                cin >> mat[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.maxIntersections(mat, N) << endl;
+    }
+}
+// } Driver Code Ends
