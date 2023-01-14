dceea24859346b1c228cfb2633172f526af0f4c7
Bipul-Harsh <bipulharsh123@gmail.com>
2023-01-14 22:03:53 +0530

    1-M

---
+++ RemoveTheBalls.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int finLength(int N, vector<int> color, vector<int> radius) {
+        stack<pair<int, int>> s;
+        for(int ind=0; ind<N; ind++){
+            int c = color[ind];
+            int r = radius[ind];
+            if(s.size()>0 && s.top().first == c && s.top().second == r){
+                s.pop();
+            } else {
+                s.push({c, r});
+            }
+        }
+        
+        return s.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+
+        int n;
+        cin >> n;
+        vector<int> color(n), radius(n);
+        for (int i = 0; i < n; i++) cin >> color[i];
+        for (int i = 0; i < n; i++) cin >> radius[i];
+        int ans = sol.finLength(n, color, radius);
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
